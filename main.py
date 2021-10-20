d1546b14ee775b304437cb2e6af42869885b0c40
Mateusz Strzelecki <mateuszstrzelecki21@gmail.com>
2021-10-20 20:22:11 +0200

    Create new entry route. Create index.html, add-password.html with html form.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -15,8 +15,11 @@ db = SQLAlchemy(app)
 
 # CREATE TABLE
 class Password(db.Model):
+    __tablename__ = "password_manager"
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(250), unique=True, nullable=False)
+    site_name = db.Column(db.String(100), nullable=False)
+    site_url = db.Column(db.String(250), nullable=False)
+    login = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
 # Run only once to create data base
@@ -25,7 +28,16 @@ class Password(db.Model):
 
 @app.route("/")
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_new_entry():
+    if request.method == "POST":
+        data = request.form
+        print(data)
+        return redirect(url_for("home"))
+    return render_template("add-password.html")
 
 
 if __name__ == '__main__':
