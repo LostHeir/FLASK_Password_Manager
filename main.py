f9d171ce3fde4e2dde0edab4453a1a44cf13342d
Mateusz Strzelecki <mateuszstrzelecki21@gmail.com>
2021-10-21 18:56:36 +0200

    Create show_all and empty edit/delete routes. Create show-all.html.

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '3TDE1-Kb7t7mCW1xt7-Yh4NF03bobu4TSZAYyo8TxBo'
+app.config["SECRET_KEY"] = "3TDE1-Kb7t7mCW1xt7-Yh4NF03bobu4TSZAYyo8TxBo"
 # Generated from terminal: python -c "import secrets; print(secrets.token_urlsafe(32))"
 
 
 # CREATE DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passwords.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///passwords.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
@@ -19,7 +19,7 @@ class Password(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     site_name = db.Column(db.String(100), nullable=False)
     site_url = db.Column(db.String(250), nullable=False)
-    login = db.Column(db.String(100), unique=True, nullable=False)
+    login = db.Column(db.String(100),  nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
 # Run only once to create data base
@@ -31,14 +31,40 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/show_all')
+def get_all_passwords():
+    passwords = Password.query.all()
+    print(passwords[0].login)
+    return render_template("show-all.html", all_passwords=passwords)
+
+
 @app.route("/add", methods=["GET", "POST"])
 def add_new_entry():
     if request.method == "POST":
         data = request.form
-        print(data)
+        new_password = Password(
+            site_name=data["site"],
+            site_url=data["site_url"],
+            login=data["login"],
+            password=data["password"]
+        )
+        db.session.add(new_password)
+        db.session.commit()
         return redirect(url_for("home"))
     return render_template("add-password.html")
 
 
-if __name__ == '__main__':
+@app.route("/edit_password/<int:password_id>", methods=["GET", "POST"])
+def edit_entry():
+    if request.method == "POST":
+        return redirect(url_for("home"))
+    return redirect(url_for("home"))
+
+
+@app.route("/delete_password/<int:password_id>")
+def edit_entry():
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
