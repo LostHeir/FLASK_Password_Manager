81b22b2860de6fdde63810a15c507c18d4e49d3e
Mateusz Strzelecki <mateuszstrzelecki21@gmail.com>
2021-10-23 18:34:24 +0200

    Final touches. Add function description.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 from sqlalchemy.orm import relationship
 from cryptography.fernet import Fernet
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import SignatureExpired, TimedJSONWebSignatureSerializer as Serializer
 
 
 # I will be using Fernet to encrypt and decrypt password stored in database.
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "3TDE1-Kb7t7mCW1xt7-Yh4NF03bobu4TSZAYyo8TxBo"
 # Generated from terminal: python -c "import secrets; print(secrets.token_urlsafe(32))"
 
-# CREATE LOGIN MANAGER
+# CREATE LOGIN MANAGER - needed for user management.
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -26,43 +26,46 @@ login_manager.init_app(app)
 def load_user(user_id):
     return PmUser.query.get(int(user_id))
 
+
 # CREATE DB
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///passwords.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+# TIME SERALIZATION
 def generate_auth_token():
+    """Generates authentication token using Timed JSON Signature.
+     Token is created with None object - there is no need to create any, only time stamp is important.
+     Retrun time serialized token."""
     s = Serializer(app.config['SECRET_KEY'], expires_in=10)
-
-    token = s.dumps({"id": "2"})
-
+    token = s.dumps(None)
     return token
 
 
 def verify_auth_token(token):
+    """Verify time stamped token. Return True till time is valid. After time expires return False."""
     s = Serializer(app.config['SECRET_KEY'])
     try:
         s.loads(token)
         return True
-    except:
+    except SignatureExpired:
         return False
 
 
 # DECORATORS
 def login_required(func):
+    """Check if current user is loged in. If not return 403 - Access denied."""
     @functools.wraps(func)
     def decorated_function(*args, **kwargs):
         if current_user.is_anonymous or current_user.id != 1:
-            print(current_user.is_anonymous, "in@T")
             return abort(403)
         else:
-            print(current_user.is_anonymous, "in@F")
             return func(*args, **kwargs)
     return decorated_function
 
 
-# CREATE TABLE
+# CREATE TABLES
 class PmUser(UserMixin, db.Model):
     __tablename__ = "pm_users"
     id = db.Column(db.Integer, primary_key=True)
@@ -95,7 +98,7 @@ class Password(db.Model):
 # ROUTES
 @app.route("/", methods=["GET", "POST"])
 def home():
-    print(current_user.is_anonymous)
+    """Home page with login form. Data in login form doesn't matter, there is only one hardcoded user in DB."""
     logout_user()  # for hardcoded user - if user don't click Login button he will be treated like anonymous one.
     if request.method == "POST":
         selected_user = PmUser.query.filter_by(id=1).first()
@@ -108,6 +111,8 @@ def home():
 @app.route('/show_all')
 @login_required
 def get_all_passwords():
+    """Shows all sites sotred in DB. Displays only site name and login used for login,
+     click for page name for more details. Passwords are decrypted and then displayed to the user."""
     passwords = Password.query.all()
     return render_template("show-all.html", all_passwords=passwords)
 
@@ -115,6 +120,7 @@ def get_all_passwords():
 @app.route("/add", methods=["GET", "POST"])
 @login_required
 def add_new_entry():
+    """Creates form to create new entry. Adds new entry to the DB. Passwords are encrypted using fernet library."""
     if request.method == "POST":
         data = request.form
         enc_password = fernet.encrypt(data["password"].encode())
@@ -132,12 +138,13 @@ def add_new_entry():
 
 @app.route('/share_entry/<token>/<int:password_id>')
 def share_entry(token, password_id):
+    """Displays shared entry details. After expire time redirect user to the home page with notification about this."""
     if verify_auth_token(token):
         entry_to_show = Password.query.get(password_id)
         password_to_show = fernet.decrypt(entry_to_show.password).decode()
         return render_template("share-password.html", data=entry_to_show, password=password_to_show)
     else:
-        flash("temp")
+        flash("Sorry Your URL has expird, ask for new one.")
         return redirect(url_for("home"))
 
 
@@ -145,6 +152,15 @@ def share_entry(token, password_id):
 @app.route("/show_details/<int:password_id>", defaults={'show_link': None, 'token': 'nulltoken'})
 @login_required
 def show_details(token, password_id, show_link):
+    """
+    Shows details of given entry (password_id). Creates buttons: Edit, Back, Share.
+    Edit button redirects user to the edit page.
+    Back button redirects user to the show all page.
+    Share button creates link, which can be sent to the guest_user.
+    Guest will have limited access to the password manager:
+    he will only be able to see given entry for a certain amount of time. (token with seralized time stamp is created)
+    After that he will be redirected to the home page.
+    """
     if request.method == "POST":
         token = generate_auth_token()
         entry_to_show = Password.query.get(password_id)
@@ -160,6 +176,7 @@ def show_details(token, password_id, show_link):
 @app.route("/edit/<int:password_id>", methods=["GET", "POST"])
 @login_required
 def edit_entry(password_id):
+    """Creates form to edit given entry. Saves changes to DB afterwards. Password is encrypted using fernet library"""
     password_to_edit = Password.query.get(password_id)
     # Decrypt password to change it
     dec_password = fernet.decrypt(password_to_edit.password).decode()
@@ -181,6 +198,7 @@ def edit_entry(password_id):
 @app.route("/delete/<int:password_id>")
 @login_required
 def delete_entry(password_id):
+    """Romoves given entry from DB."""
     password_to_delete = Password.query.get(password_id)
     db.session.delete(password_to_delete)
     db.session.commit()
