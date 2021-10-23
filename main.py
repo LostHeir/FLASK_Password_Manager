8a2fb3d862e0dcc62da3c7583c8e20bf00c1eb2e
Mateusz Strzelecki <mateuszstrzelecki21@gmail.com>
2021-10-23 13:51:19 +0200

    Add User management need for sharing entries to anonymous ones. Change method to post in index form.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from sqlalchemy.orm import relationship
 from cryptography.fernet import Fernet
 
-# I will be using Ferner to encrypt and decrypt password stored in database.
+
+# I will be using Fernet to encrypt and decrypt password stored in database.
 # Encryption key is needed, I will use Fernet to generate such key, it can be done by using other random key generators.
 key = "SW3HojFnjgC80mLvthtAcKjaELM_qNJkooSYIKK5GxY=".encode()  # key generated with: Fernet.generate_key()
 fernet = Fernet(key)
@@ -11,6 +14,15 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "3TDE1-Kb7t7mCW1xt7-Yh4NF03bobu4TSZAYyo8TxBo"
 # Generated from terminal: python -c "import secrets; print(secrets.token_urlsafe(32))"
 
+# CREATE LOGIN MANAGER
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# USER LOADER - needed for user management.
+@login_manager.user_loader
+def load_user(user_id):
+    return PmUser.query.get(int(user_id))
 
 # CREATE DB
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///passwords.db"
@@ -19,13 +31,30 @@ db = SQLAlchemy(app)
 
 
 # CREATE TABLE
+class PmUser(UserMixin, db.Model):
+    __tablename__ = "pm_users"
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    # Create realtion One to Many with Password table
+    entries = relationship("Password", back_populates="author")
+
+
 class Password(db.Model):
     __tablename__ = "password_manager"
     id = db.Column(db.Integer, primary_key=True)
+
     site_name = db.Column(db.String(100), nullable=False)
     site_url = db.Column(db.String(250), nullable=False)
     login = db.Column(db.String(100),  nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+    # Create relation to BlogUser table
+    author = relationship("PmUser", back_populates="entries")
+    # Create foreign key to identify author
+    author_id = db.Column(db.Integer, db.ForeignKey("pm_users.id"))
 
 # Run only once to create data base
 # db.create_all()
@@ -34,8 +63,12 @@ class Password(db.Model):
 # ROUTES
 @app.route("/", methods=["GET", "POST"])
 def home():
+    logout_user()  # for hardcoded user - if user don't click Login button he will be treated like anonymous one.
     if request.method == "POST":
-        redirect(url_for("get_all_passwords"))
+        selected_user = PmUser.query.filter_by(id=1).first()
+        login_user(selected_user)
+        passwords = Password.query.all()
+        return render_template("show-all.html", all_passwords=passwords)
     return render_template("index.html")
 
 
